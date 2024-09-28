ead9f029244a6f4decc0d1aa26049b3b8f437bb3
Lucas Bernadino <luucasdinoo@gmail.com>
2024-09-28 19:45:32 -0300

    Crud simples para genres utilizando class based views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from genres.views import GenreCreateListView, GenreRetrievelUpdateDestroyView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres/', GenreCreateListView.as_view() , name='genre-create-list'),
+    path('genres/<int:pk>', GenreRetrievelUpdateDestroyView.as_view(), name='genre-update-detail-destroy'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from genres.models import Genre
+from genres.serializers import GenreSerializer
 
-# Create your views here.
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreRetrievelUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
